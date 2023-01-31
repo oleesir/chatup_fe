841baa86c74a54adb2bd1c94690e5d607b5f5d63
oleesir <oleesir@gmail.com>
2023-01-31 02:32:49 -0500

    initialize apolloclient

---
+++ src/graphql/apollo-client.ts
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+export const httpLink = new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+    credentials: "include",
+});
+export const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+});
